6f9507a31152f9f617d4a3d262d1057786d274ae
Horia Iacos <iacos.horia@gmail.com>
2023-07-19 15:55:05 +0300

    feat: Add vpc module

--- main.tf
+++ main.tf
@@ -19,3 +19,24 @@ locals {
 }
 
 data "aws_caller_identity" "current" {}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "${local.namespace}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = local.tags
+}
