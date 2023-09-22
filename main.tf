d29d3e3f15f256aa1c00826269fb2dd4ba153e3c
Horia Iacos <iacos.horia@gmail.com>
2023-09-22 12:42:06 +0300

    fix: Password policy configured for infrastructure [AVMTECH-510]

--- main.tf
+++ main.tf
@@ -651,6 +651,15 @@ resource "aws_wafv2_web_acl" "web_acl_alb" {
   tags = local.tags
 }
 
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
+
 ################################################################################
 # Server Module
 ################################################################################
